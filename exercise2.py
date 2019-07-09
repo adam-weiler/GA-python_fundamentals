cfda5844d792e3d11adbaefab76455778de88eff
adam-weiler <adam.weiler@yahoo.ca>
2019-07-09 13:35:21 -0400

    Adding completed Exercise 2.

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+#Exercise 2
+
+#Question 1: What is a good tip for $55?
+print(55 * .15)
+#The output is 8.25
+
+#Question 2: 
+print('Our string' + str(42))
+#The output is 'string42'
+
+#Question 3:
+print("The solution is: {}".format(45628 * 7839))
+#The output is 'The solution is: 357677892'
+
+#Question 4:
+print((10 < 20 and 30 < 20) or not (10 == 11))
+#The output is True
